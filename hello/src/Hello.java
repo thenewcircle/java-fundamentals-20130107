bbcb460d1f4a69e4004894d6565bd08183f0ef9b
Dan Rosen <dan@marakana.com>
2013-01-07 15:15:41 -0800

    added a while-loop

--- hello/src/Hello.java
+++ hello/src/Hello.java
@@ -1,9 +1,13 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		if (args.length == 1) {
-			String name = args[0];
-			System.out.println("Hello, " + name);
+		if (args.length != 0) {
+			int i = 0;
+			while (i < args.length) {
+				String name = args[i];
+				System.out.println("Hello, " + name);
+				i++;
+			}
 		} else {
 			System.out.println("Hello, Nobody!");
 		}
