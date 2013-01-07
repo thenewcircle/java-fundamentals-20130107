3f6f327b2a0cb3960e035f85ceff4197ba271b86
Dan Rosen <dan@marakana.com>
2013-01-07 14:36:49 -0800

    handle ArrayIndexOutOfBoundsException in hello world

--- hello/src/Hello.java
+++ hello/src/Hello.java
@@ -1,8 +1,12 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		String name = args[0];
-		System.out.println("Hello, " + name);
+		try {
+			String name = args[0];
+			System.out.println("Hello, " + name);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Hello, Nobody!");
+		}
 	}
 
 }
