32c52fc5d805d08831fea68ed6c7fb191e8d73d8
Dan Rosen <dan@marakana.com>
2013-01-07 16:28:04 -0800

    added calculator project outline

---
+++ calculator/src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+	public static void main(String[] args) {
+
+		// TODO: get an expression from the command line args
+
+		// split expression up into tokens
+		// for each token:
+		//   if we see a number:
+		//     - push it on the stack
+		//   if we see an operator:
+		//     - pop two numbers
+		//     - perform the op
+		//     - push the result
+		// the result is the last thing left on the stack
+
+	}
+
+}
