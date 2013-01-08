21adcdbfdc5934ac8a46e0af92883251c6718b42
Dan Rosen <dan@marakana.com>
2013-01-08 11:00:49 -0800

    made handleNumber and handleOperator independent, but with a logic bug...

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -17,7 +17,10 @@ public class Calculator {
 		// for each token in the expression ...
 		Stack<Integer> stack = new Stack<Integer>();
 		for (String token : tokens) {
-			handleNumber(stack, token);
+			if (!handleOperator(stack, token) &&
+				!handleNumber(stack, token)) {
+				throw new IllegalArgumentException("garbage");
+			}
 		}
 
 		// the result is the last thing left on the stack
@@ -25,17 +28,18 @@ public class Calculator {
 		System.out.println("result: " + result);
 	}
 
-	public static void handleNumber(Stack<Integer> stack, String token) {
+	public static boolean handleNumber(Stack<Integer> stack, String token) {
 		try {
 			// if the token is an integer, push it
 			int number = Integer.parseInt(token);
 			stack.push(number);
+			return true;
 		} catch (NumberFormatException e) {
-			handleOperator(stack, token);
+			return false;
 		}
 	}
 
-	public static void handleOperator(Stack<Integer> stack, String token) {
+	public static boolean handleOperator(Stack<Integer> stack, String token) {
 		// if the token is an operator, pop two numbers,
 		// perform the op and push the result
 		int rhs = stack.pop(), lhs = stack.pop();
@@ -48,8 +52,9 @@ public class Calculator {
 		} else if (token.equals("/")) {
 			stack.push(lhs / rhs);
 		} else {
-			throw new IllegalArgumentException("garbage in expression");
+			return false;
 		}
+		return true;
 	}
 
 }
