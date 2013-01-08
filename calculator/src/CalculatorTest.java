ba80982df8af037c45c0253511de992e7a4674e6
Dan Rosen <dan@marakana.com>
2013-01-08 12:19:01 -0800

    added outline for calculator test

---
+++ calculator/src/CalculatorTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CalculatorTest {
+
+	/*
+	 * TODO:
+	 *   handleNumber:
+	 *     should handle number: push number and return true
+	 *     should not handle non-number: do nothing and return false
+	 *     
+	 *   handleOperator
+	 *     should handle operator: pop, op, push and return true
+	 *     should not handle non-operator: do nothing and return false
+	 *     
+	 *   calculate
+	 *     integration test: result is calculated correctly
+	 *     if you pass garbage, it should throw an exception
+	 */
+
+	@Test
+	public void test() {
+		assertEquals(2, 1 + 1);
+	}
+
+}
