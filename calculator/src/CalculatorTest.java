bc7b9a169a7f31065e8c68c5cb9bcd59324adc40
Dan Rosen <dan@marakana.com>
2013-01-08 12:51:50 -0800

    finished unit tests for calculator

--- calculator/src/CalculatorTest.java
+++ calculator/src/CalculatorTest.java
@@ -31,4 +31,40 @@ public class CalculatorTest {
 		assertEquals(5, (int) stack.peek());
 	}
 
+	@Test
+	public void handleNumberMustNotHandleGarbage() {
+		Stack<Integer> stack = new Stack<Integer>();
+		boolean result = Calculator.handleNumber(stack, "garbage");
+		assertFalse(result);
+		assertTrue(stack.isEmpty());
+	}
+
+	@Test
+	public void handleOperatorMustHandleAddition() {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(3);
+		stack.push(5);
+		boolean result = Calculator.handleOperator(stack, "+");
+		assertTrue(result);
+		assertEquals(1, stack.size());
+		assertEquals(8, (int) stack.peek());
+	}
+
+	@Test
+	public void handleOperatorMustNotHandleGarbage() {
+		Stack<Integer> stack = new Stack<Integer>();
+		boolean result = Calculator.handleOperator(stack, "garbage");
+		assertFalse(result);
+		assertTrue(stack.isEmpty());
+	}
+
+	@Test
+	public void onePlusOneMustEqualTwo() {
+		assertEquals(2, Calculator.calculate("1 1 +"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void calculatorMustNotAcceptGarbage() {
+		Calculator.calculate("garbage");
+	}
 }
