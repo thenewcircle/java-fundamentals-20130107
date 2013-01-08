af9bafc3f669712edd073d60ea1ad1ec04bba0bb
Dan Rosen <dan@marakana.com>
2013-01-08 10:26:40 -0800

    some preliminary refactoring of calculator

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -17,31 +17,39 @@ public class Calculator {
 		// for each token in the expression ...
 		Stack<Integer> stack = new Stack<Integer>();
 		for (String token : tokens) {
-			try {
-				// if the token is an integer, push it
-				int number = Integer.parseInt(token);
-				stack.push(number);
-			} catch (NumberFormatException e) {
-				// if the token is an operator, pop two numbers,
-				// perform the op and push the result
-				int rhs = stack.pop(), lhs = stack.pop();
-				if (token.equals("+")) {
-					stack.push(lhs + rhs);
-				} else if (token.equals("-")) {
-					stack.push(lhs - rhs);
-				} else if (token.equals("*")) {
-					stack.push(lhs * rhs);
-				} else if (token.equals("/")) {
-					stack.push(lhs / rhs);
-				} else {
-					throw new IllegalArgumentException("garbage in expression");
-				}
-			}
+			handleNumber(stack, token);
 		}
 
 		// the result is the last thing left on the stack
 		int result = stack.pop();
 		System.out.println("result: " + result);
+	}
+
+	public static void handleNumber(Stack<Integer> stack, String token) {
+		try {
+			// if the token is an integer, push it
+			int number = Integer.parseInt(token);
+			stack.push(number);
+		} catch (NumberFormatException e) {
+			handleOperator(stack, token);
+		}
+	}
+
+	public static void handleOperator(Stack<Integer> stack, String token) {
+		// if the token is an operator, pop two numbers,
+		// perform the op and push the result
+		int rhs = stack.pop(), lhs = stack.pop();
+		if (token.equals("+")) {
+			stack.push(lhs + rhs);
+		} else if (token.equals("-")) {
+			stack.push(lhs - rhs);
+		} else if (token.equals("*")) {
+			stack.push(lhs * rhs);
+		} else if (token.equals("/")) {
+			stack.push(lhs / rhs);
+		} else {
+			throw new IllegalArgumentException("garbage in expression");
+		}
 	}
 
 }
