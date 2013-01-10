9b0d176fbd641c7f766da26bf4583ebebc4b5d6f
Dan Rosen <dan@marakana.com>
2013-01-10 12:14:32 -0800

    Storage is now an abstract base class, Disk extends Storage

--- objects/src/com/marakana/objects/Disk.java
+++ objects/src/com/marakana/objects/Disk.java
@@ -1,48 +1,18 @@
 package com.marakana.objects;
 
-public class Disk implements Storage {
-	private final int size;
-	private final String type;
+public class Disk extends Storage {
+	private final int speed;
 
-	public Disk(int size, String type) {
-		this.size = size;
-		this.type = type;
+	public Disk(int size, String type, int speed) {
+		super(size, type);
+		this.speed = speed;
+	}
+
+	public int getSpeed() {
+		return speed;
 	}
 
 	public int getSize() {
 		return size;
 	}
-
-	public String getType() {
-		return type;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + size;
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Disk other = (Disk) obj;
-		if (size != other.size)
-			return false;
-		if (type == null) {
-			if (other.type != null)
-				return false;
-		} else if (!type.equals(other.type))
-			return false;
-		return true;
-	}
-
 }
--- objects/src/com/marakana/objects/Storage.java
+++ objects/src/com/marakana/objects/Storage.java
@@ -1,6 +1,47 @@
 package com.marakana.objects;
 
-public interface Storage {
-	int getSize();
-	String getType();
+public abstract class Storage {
+	protected final int size;
+	protected final String type;
+
+	public Storage(int size, String type) {
+		this.size = size;
+		this.type = type;
+	}
+
+	public abstract int getSize();
+
+	public String getType() {
+		return type;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + size;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Storage))
+			return false;
+		Storage other = (Storage) obj;
+		if (size != other.size)
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+
 }
