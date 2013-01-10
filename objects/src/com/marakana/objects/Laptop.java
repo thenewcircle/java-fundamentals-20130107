6f4663ed126e8d9e01cf04c4236246ddeb39e25a
Dan Rosen <dan@marakana.com>
2013-01-10 10:41:54 -0800

    changed arrays to standard collections

--- objects/src/com/marakana/objects/Laptop.java
+++ objects/src/com/marakana/objects/Laptop.java
@@ -1,15 +1,16 @@
 package com.marakana.objects;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class Laptop {
 	private final String model;
 	private final int size;
-	private final Storage[] storage;
-	private String[] applications;
+	private final List<Storage> storage;
+	private Set<String> applications;
 	private boolean on;
 
-	public Laptop(String model, int size, Storage[] storage) {
+	public Laptop(String model, int size, List<Storage> storage) {
 		this.model = model;
 		this.size = size;
 		this.storage = storage;
@@ -35,15 +36,15 @@ public class Laptop {
 		return size;
 	}
 
-	public Storage[] getStorage() {
+	public List<Storage> getStorage() {
 		return storage;
 	}
 
-	public String[] getApplications() {
+	public Set<String> getApplications() {
 		return applications;
 	}
 
-	public void setApplications(String[] applications) {
+	public void setApplications(Set<String> applications) {
 		this.applications = applications;
 	}
 
@@ -61,7 +62,7 @@ public class Laptop {
 		int result = 1;
 		result = prime * result + ((model == null) ? 0 : model.hashCode());
 		result = prime * result + size;
-		result = prime * result + Arrays.hashCode(storage);
+		result = prime * result + ((storage == null) ? 0 : storage.hashCode());
 		return result;
 	}
 
@@ -81,7 +82,10 @@ public class Laptop {
 			return false;
 		if (size != other.size)
 			return false;
-		if (!Arrays.equals(storage, other.storage))
+		if (storage == null) {
+			if (other.storage != null)
+				return false;
+		} else if (!storage.equals(other.storage))
 			return false;
 		return true;
 	}
--- objects/src/com/marakana/objects/ObjectsTest.java
+++ objects/src/com/marakana/objects/ObjectsTest.java
@@ -1,6 +1,11 @@
 package com.marakana.objects;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -8,13 +13,17 @@ public class ObjectsTest {
 
 	@Test
 	public void test() {
-		Storage[] storage = {
-				new Memory(4096, "DIMM"),
-				new Memory(4096, "DIMM"),
-				new Disk(512, "SATA")
-			};
+		List<Storage> storage = new ArrayList<Storage>();
+		storage.add(new Memory(4096, "DIMM"));
+		storage.add(new Memory(4096, "DIMM"));
+		storage.add(new Disk(512, "SATA"));
 
 		Laptop lappy = new Laptop("MacBook Pro", 15, storage);
+		Set<String> applications = new HashSet<String>();
+		applications.add("Eclipse");
+		applications.add("BitTorrent");
+		lappy.setApplications(applications);
+
 		assertEquals(520, lappy.totalStorage());
 
 		lappy.turnOn();
