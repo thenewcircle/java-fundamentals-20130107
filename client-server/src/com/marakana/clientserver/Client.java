5136d892789f8b9fcb6bb0a17d9121e218dc220f
Dan Rosen <dan@marakana.com>
2013-01-11 12:55:13 -0800

    added client-server with resource leaks

---
+++ client-server/src/com/marakana/clientserver/Client.java
@@ -0,0 +1,21 @@
+package com.marakana.clientserver;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) {
+		try {
+			Socket server = new Socket(InetAddress.getLocalHost(), 31337);
+			PrintWriter out = new PrintWriter(server.getOutputStream());
+			out.println("Hello, Server!");
+			out.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ client-server/src/com/marakana/clientserver/Server.java
@@ -0,0 +1,23 @@
+package com.marakana.clientserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) {
+		try {
+			ServerSocket server = new ServerSocket(31337);
+			Socket client = server.accept();
+			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			String message = in.readLine();
+			System.out.println(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
